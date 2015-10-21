13926451cf9f0e2390685ddafb389ebd051e7cf7
Michael Wetherald <michaelwetherald@gmail.com>
2015-10-20 20:19:44 -0700

    add first server tests

--- specs/serverSpecs/serverSpec.js
+++ specs/serverSpecs/serverSpec.js
@@ -1,13 +1,57 @@
 var chai = require('chai');
+var request = require('request');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/locket');
+var User = require('../../server/features/users/userModel');
 
 var assert = chai.assert;
 var should = chai.should();
 var expect = chai.expect;
 
-describe("server tests", function(){
+var IP = 'http://127.0.0.1:1337';
+
+describe('server tests', function(){
+
+  var requestWithSession = request.defaults({jar: true});
+
+  beforeEach(function() {
+
+  });
+
+  it('should respond to a get request', function(done){
+    request(IP, function(error, res, body) {
+
+      expect(error).to.be.null;
+      expect(body).to.have.string('<html');
+
+      done();
+    });
+  });
   
-  it('should pass a test', function(){
-    assert(true);
+  describe('API test', function(){
+    it('should allow users to signup', function(done){
+      //remove existing testUser
+      User.findOneAndRemove({username:"testUser"}, null, function(err, user){
+        var options = {
+          'method': 'POST',
+          'uri': IP + '/api/users/signup',
+          'json': {
+            username: "testUser", 
+            password: "testPass"
+          }
+        };
+        request.post(options, function(error, res, body) {
+
+          console.log(body);
+          expect(error).to.be.null;
+
+          done();
+        });
+      });
+      
+    });
+
   });
   
 });
